85475f325720423014676f0a9a6a52e09c2d5a2b
celeron533 <zhuyilun8@hotmail.com>
2021-01-25 12:37:56 +0800

    Display the Filename name instead of the full path

--- DicomGrep/Models/ResultDicomFile.cs
+++ DicomGrep/Models/ResultDicomFile.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace DicomGrep.Models
 {
     public class ResultDicomFile
     {
-        public string Filename { get; private set; }
+        public string FullFilename { get; private set; }
+        public string Filename => Path.GetFileName(FullFilename);
+        public string DirectoryName => Path.GetDirectoryName(FullFilename);
+
         public string SOPClassName { get; private set; }
         public string SOPClassUID { get; private set; }
         public string PatientName { get; private set; }
@@ -14,7 +18,7 @@ namespace DicomGrep.Models
 
         public ResultDicomFile(string filename, string SOPClassName, string SOPClassUID, string PatientName, IList<ResultDicomItem> resultDicomItems)
         {
-            this.Filename = filename;
+            this.FullFilename = filename;
             this.SOPClassName = SOPClassName;
             this.SOPClassUID = SOPClassUID;
             this.ResultDicomItems = resultDicomItems;
